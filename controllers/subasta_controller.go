57ed9cc7b3fc575cf602973290d1d142b6ab2e58
mauricioponguta <pongutaerit549@gmail.com>
2026-04-30 14:15:34 -0500

    Se crea routes de subasta

--- controllers/subasta_controller.go
+++ controllers/subasta_controller.go
@@ -1,3 +1,88 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"agropecuario_crud/config"
+	"agropecuario_crud/models"
+
+	"github.com/gorilla/mux"
+)
+
+
+// GET ALL
+func GetSubastas(w http.ResponseWriter, r *http.Request) {
+	query := `
+	SELECT id_subasta, fecha_subasta, ubicacion, precio,
+	       id_usuario_registra, activo, fecha_creacion, fecha_modificacion
+	FROM agropecuario.subasta
+	`
+
+	rows, err := config.DB.Query(query)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer rows.Close()
+
+	var subastas []models.Subasta
+
+	for rows.Next() {
+		var s models.Subasta
+		rows.Scan(
+			&s.ID,
+			&s.Fecha,
+			&s.Ubicacion,
+			&s.Precio,
+			&s.IdUsuarioRegistra,
+			&s.Activo,
+			&s.FechaCreacion,
+			&s.FechaModificacion,
+		)
+		subastas = append(subastas, s)
+	}
+
+	json.NewEncoder(w).Encode(subastas)
+}
+
+
+// GET BY ID
+func GetSubastaByID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	query := `
+	SELECT id_subasta, fecha_subasta, ubicacion, precio,
+	       id_usuario_registra, activo, fecha_creacion, fecha_modificacion
+	FROM agropecuario.subasta
+	WHERE id_subasta = $1
+	`
+
+	var s models.Subasta
+
+	err := config.DB.QueryRow(query, id).Scan(
+		&s.ID,
+		&s.Fecha,
+		&s.Ubicacion,
+		&s.Precio,
+		&s.IdUsuarioRegistra,
+		&s.Activo,
+		&s.FechaCreacion,
+		&s.FechaModificacion,
+	)
+
+	if err != nil {
+		http.Error(w, "No encontrado", 404)
+		return
+	}
+
+	json.NewEncoder(w).Encode(s)
+}
+
+
+// CREATE
 func CreateSubasta(w http.ResponseWriter, r *http.Request) {
 	var s models.Subasta
 
@@ -29,4 +114,59 @@ func CreateSubasta(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(s)
+}
+
+
+// UPDATE
+func UpdateSubasta(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	var s models.Subasta
+	json.NewDecoder(r.Body).Decode(&s)
+
+	query := `
+	UPDATE agropecuario.subasta
+	SET fecha_subasta=$1,
+	    ubicacion=$2,
+	    precio=$3,
+	    id_usuario_registra=$4,
+	    activo=$5,
+	    fecha_modificacion=now()
+	WHERE id_subasta=$6
+	`
+
+	_, err := config.DB.Exec(
+		query,
+		s.Fecha,
+		s.Ubicacion,
+		s.Precio,
+		s.IdUsuarioRegistra,
+		s.Activo,
+		id,
+	)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "actualizado"})
+}
+
+
+// DELETE (físico)
+func DeleteSubasta(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	query := `DELETE FROM agropecuario.subasta WHERE id_subasta=$1`
+
+	_, err := config.DB.Exec(query, id)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{"message": "eliminado"})
 }
--- routes/subasta_routes.go
+++ routes/subasta_routes.go
@@ -8,10 +8,8 @@ import (
 func SubastaRoutes(r *mux.Router) {
 
 	r.HandleFunc("/subastas", controllers.GetSubastas).Methods("GET")
+	r.HandleFunc("/subastas/{id}", controllers.GetSubastaByID).Methods("GET")
 	r.HandleFunc("/subastas", controllers.CreateSubasta).Methods("POST")
 	r.HandleFunc("/subastas/{id}", controllers.UpdateSubasta).Methods("PUT")
 	r.HandleFunc("/subastas/{id}", controllers.DeleteSubasta).Methods("DELETE")
-
-	r.HandleFunc("/relaciones", controllers.GetRelaciones).Methods("GET")
-	r.HandleFunc("/relaciones", controllers.CreateRelacion).Methods("POST")
 }
